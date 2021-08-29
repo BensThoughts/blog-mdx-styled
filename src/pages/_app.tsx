6a429744e0fb391a922b931f33d4c7dece117671
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-28 18:10:39 -0700

    added fragment

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from '@app/utils/colorMode';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
       <Component {...pageProps} />
+    </>
   );
 }
 export default MyApp
