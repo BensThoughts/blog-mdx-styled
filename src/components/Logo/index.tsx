312607960e6391b48158732fdfc0230f59fdbba4
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-28 18:10:01 -0700

    Logo component added

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function Logo() {
+  return (
+    <>
+      <Image src="/assets/logo/logo.png" alt="logo" width="25" height="25" />
+    </>
+  );
+}
