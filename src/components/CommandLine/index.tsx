2bbe55c0fb1d3dea3e9ea0ad2071f26246e1f16b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-31 23:04:32 -0700

    CommandLine respects the grid width

--- src/components/CommandLine/index.tsx
+++ src/components/CommandLine/index.tsx
@@ -59,7 +59,7 @@ export default function CommandLine(props: CommandLineProps) {
       (options ? options + ' ' : '') +
       (args ? args : '');
   return (
-    <CommandLineBorder className="m-auto align-middle sm:border-solid sm:border-opacity-20 sm:border-2 rounded-md p-1 sm:p-2">
+    <CommandLineBorder className="m-auto max-w-full align-middle sm:border-solid sm:border-opacity-20 sm:border-2 rounded-md p-1 sm:p-2">
       <Pre className="overflow-x-auto rounded-sm px-4 py-2">
         <div className="flex flex-row justify-between items-center">
           <div>
--- src/components/mdx/H2/index.tsx
+++ src/components/mdx/H2/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function H1(props: { children: ReactNode }) {
   return (
-    <h2 className="text-lg m-auto">
+    <h2 className="text-lg sm:text-2xl m-auto">
       {props.children}
     </h2>
   );
