3c68bb6938f0801989746dea3c0c7fefaaab01c7
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-01 13:30:31 -0700

    implemented IconButton

---
+++ src/components/IconButton/index.tsx
@@ -0,0 +1,13 @@
+type IconButtonProps = {
+  children: React.ReactChild;
+  onClick: any;
+  className: string;
+}
+
+export default function IconButton({ className, children, onClick }: IconButtonProps) {
+  return (
+    <button onClick={onClick} className={`bg-secondary font-bold py-2 px-4 rounded inline-flex items-center ${className}`}>
+      {children}
+    </button>
+  )
+}
