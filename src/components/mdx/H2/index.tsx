2de58ddb0a045fb2ce8dc3c1cddcbf69bb9c8761
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-07 11:21:53 -0700

    better margin and color

--- src/components/mdx/H2/index.tsx
+++ src/components/mdx/H2/index.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 
 export default function H1(props: { children: ReactNode }) {
   return (
-    <h2 className="text-xl md:text-2xl mx-auto my-5">
+    <h2 className="text-xl md:text-2xl mx-auto my-3 text-secondary">
       {props.children}
     </h2>
   );
