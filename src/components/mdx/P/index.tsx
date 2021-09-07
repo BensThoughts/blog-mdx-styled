6949c4eb2f10841fc6e658c0ab9dc227ad7ba8b5
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-07 12:35:56 -0700

    easier on the eyes

--- src/components/mdx/P/index.tsx
+++ src/components/mdx/P/index.tsx
@@ -2,6 +2,6 @@ import { ReactNode } from 'react';
 
 export default function P(props: { children: ReactNode}) {
   return (
-    <p className="mx-auto my-6 w-auto text-base leading-loose">{props.children}</p>
+    <p className="mx-auto my-6 w-auto text-base sm:text-lg text-opacity-90 text-primary leading-loose">{props.children}</p>
   );
 };
