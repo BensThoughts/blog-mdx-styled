ee0b0a529a1b30048e16272b370dc9fcf5ee2998
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-08 15:14:00 -0700

    better social icons

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -17,26 +17,36 @@ type FooterProps = {
 export default function Footer({ className }: FooterProps) {
   return (
     <Container className={`bg-primary w-full flex flex-row justify-center items-center ${className}`}>
-      <a href="https://github.com/bensthoughts" target="_blank" rel="noreferrer">
-        <div className="rounded-xl shadow-xl mx-3 border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
-          <FontAwesomeIcon icon={['fab', 'github']} size="3x" className="mx-1 p-2 rounded-full text-icon-secondary" />
-        </div>
-      </a>
-      <a href="https://twitter.com/bensthoughts" target="_blank" rel="noreferrer">
-        <div className="rounded-xl shadow-xl mx-3 border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
-          <FontAwesomeIcon icon={['fab', 'twitter']} size="3x" className="mx-1 p-2 rounded-full text-icon-secondary" />
-        </div>
-      </a>
-      <a href="https://www.linkedin.com/in/benjaminblumenfeldjones" target="_blank" rel="noreferrer">
-        <div className="rounded-xl shadow-xl mx-3 border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
-          <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x" className="mx-1 p-2 rounded-full text-icon-secondary" />
-        </div>
-      </a>
-      <a href="https://www.facebook.com/benjamin.blumenfeldjones.9" target="_blank" rel="noreferrer">
-        <div className="rounded-xl shadow-xl mx-3 border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
-          <FontAwesomeIcon icon={['fab', 'facebook']} size="3x" className="mx-1 p-2 rounded-full text-icon-secondary" />
-        </div>
-      </a>
+      <div className="mx-3">
+        <a href="https://github.com/bensthoughts" target="_blank" rel="noreferrer">
+          <div className="rounded-xl shadow-xl border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
+            <FontAwesomeIcon icon={['fab', 'github']} size="3x" className="p-2 rounded-full text-icon-secondary" />
+          </div>
+        </a>
+      </div>
+      <div className="mx-3">
+        <a href="https://twitter.com/bensthoughts" target="_blank" rel="noreferrer">
+          <div className="rounded-xl shadow-xl border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
+            <FontAwesomeIcon icon={['fab', 'twitter']} size="3x" className="p-2 rounded-full text-icon-secondary" />
+          </div>
+        </a>
+      </div>
+
+      <div className="mx-3">
+        <a href="https://www.linkedin.com/in/benjaminblumenfeldjones" target="_blank" rel="noreferrer">
+          <div className="rounded-xl shadow-xl border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
+            <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x" className="p-2 mx-1 rounded-full text-icon-secondary" />
+          </div>
+        </a>
+      </div>
+      <div className="mx-3">
+        <a href="https://www.facebook.com/benjamin.blumenfeldjones.9" target="_blank" rel="noreferrer">
+          <div className="rounded-xl shadow-xl border-2 border-icon-primary border-opacity-50 border-solid bg-primary bg-opacity-50">
+            <FontAwesomeIcon icon={['fab', 'facebook']} size="3x" className="p-2 rounded-full text-icon-secondary" />
+          </div>
+        </a>
+      </div>
+
     </Container>
   );
 }
