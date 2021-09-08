5fd40260cf126efd4318f72850283e67be982972
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-08 13:46:38 -0700

    added react-use

--- src/components/AvatarCard/index.tsx
+++ src/components/AvatarCard/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+import { useTimeoutFn } from 'react-use';
 import H1 from '@app/components/mdx/H1';
+import Button from '../Button';
 
 export default function AvatarCard() {
   const bustImgSrc = "https://res.cloudinary.com/bensthoughts/image/upload/q_auto/v1631126786/blog/home/bust_clean_xugkq8.jpg";
+  const [isShowing, setIsShowing] = useState(true);
+  const [, , resetIsShowing] = useTimeoutFn(() => setIsShowing(true), 500);
+
   return (
-    <>
-      <img src={bustImgSrc} alt="Bust Image" className="object-cover w-48 h-48  md:w-96 md:h-96 rounded-full z-20 p-2 bg-app-bg shadow-sm"/>
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="z-20 w-48 h-48 md:w-96 md:h-96 rounded-full p-2 bg-app-bg shadow-sm">
+        <img src={bustImgSrc} alt="Bust Image" className="rounded-full"/>
+      </div>
       <div className="bg-primary w-full max-w-2xl h-60 -my-20 py-20 md:-my-36 md:py-40 z-10 rounded-lg shadow-xl">
         <div className="flex flex-col items-center justify-center w-full max-w-max mx-auto">
           <div className="flex flex-col items-center justify-center content-center md:flex-row mx-3">
@@ -12,8 +20,12 @@ export default function AvatarCard() {
             <H1 className="italic">&nbsp;Blumenfeld-Jones</H1>
           </div>
           <h2 className="italic self-start my-2 mx-3 text-center md:text-left text-lg">Self taught software developer with a passion for learning.</h2>
+          <Button onClick={() => {
+            setIsShowing(false)
+            resetIsShowing()
+          }}>Spin</Button>
         </div>
       </div>
-    </>
+    </div>
   );
 }
