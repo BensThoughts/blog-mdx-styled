73959eae277ca85627ecb4530bf84e119b8ec73b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-22 00:14:14 -0700

    styled h2 element

--- src/components/mdx/H2/index.tsx
+++ src/components/mdx/H2/index.tsx
@@ -1,11 +1,31 @@
+import styled from '@emotion/styled';
+
+const H2Styled = styled.h2`
+  position: relative;
+  padding: 0.2em 0;
+  overflow: hidden;
+  text-decoration: none;
+  color: rgb(var(--color-text-secondary));
+
+  &::after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 0.05rem;
+    background-color: rgb(var(--color-app-secondary));
+  }
+`;
+
 type H2Props = {
   children: string;
 } & React.HTMLAttributes<HTMLHeadingElement>;
 
-export default function H1({children, className}: H2Props) {
+export default function H2({children, className}: H2Props) {
   return (
-    <h2 className={`text-xl md:text-2xl mx-auto my-3 text-secondary ${className}`}>
+    <H2Styled className={`text-xl md:text-2xl mx-auto text-secondary ${className}`}>
       {children}
-    </h2>
+    </H2Styled>
   );
 };
