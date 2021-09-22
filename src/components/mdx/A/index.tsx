7d22e56a6fe5da16a7d3f0cc36d959fc1c9d3208
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-21 23:03:23 -0700

    removed underline

--- src/components/mdx/A/index.tsx
+++ src/components/mdx/A/index.tsx
@@ -7,6 +7,6 @@ type AProps = {
 
 export default function A({href, children}: AProps) {
   return (
-    <AnimatedLink href={href} className="underline text-secondary" target="_blank" rel="noreferrer noopener">{children}</AnimatedLink>
+    <AnimatedLink href={href} className="text-secondary" target="_blank" rel="noreferrer noopener">{children}</AnimatedLink>
   );
 }
