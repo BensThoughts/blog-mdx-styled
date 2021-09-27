08a60b451fca25e5042525f45489f1eecea67c21
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-09-26 18:47:30 -0700

    removed traces of react fontawesome

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,33 +10,6 @@ const ThemeProvider = dynamic(() => import('@app/utils/context/colorMode'), {
 
 import FeatureToggle from '@app/components/FeatureToggle/FeatureToggle';
 
-// import {library} from '@fortawesome/fontawesome-svg-core';
-// import {faCopy} from '@fortawesome/free-regular-svg-icons';
-// import {
-//   faBars,
-//   faDesktop,
-//   faCloudMoon,
-//   faHandSparkles,
-//   faSun,
-// } from '@fortawesome/free-solid-svg-icons';
-// import {
-//   faTwitter,
-//   faFacebook,
-//   faLinkedin,
-//   faGithub
-// } from '@fortawesome/free-brands-svg-icons';
-
-// library.add(faCopy);
-// library.add(faBars);
-// library.add(faDesktop);
-// library.add(faCloudMoon);
-// library.add(faHandSparkles);
-// library.add(faSun);
-// library.add(faTwitter);
-// library.add(faFacebook);
-// library.add(faLinkedin);
-// library.add(faGithub);
-
 
 import {Provider} from 'react-redux';
 import {DefaultSeo} from 'next-seo';
