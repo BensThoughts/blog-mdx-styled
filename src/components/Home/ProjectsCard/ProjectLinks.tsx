4ff7af1f5e247606796075cfb481c59a72ebb35f
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-02 21:01:33 -0700

    linted

--- src/components/Home/ProjectsCard/ProjectLinks.tsx
+++ src/components/Home/ProjectsCard/ProjectLinks.tsx
@@ -12,9 +12,25 @@ export default function ProjectLinks({
 }: ProjectLinksProps) {
   return (
     <div className="pt-2 px-2">
-      <div className="flex gap-4">
-        <a href={githubLink} target="_blank" rel="noopener noreferrer"><Github /></a>
-        <a href={liveLink} target="_blank" rel="noopener noreferrer"><ExternalLink /></a>
+      <div className="flex gap-4 font-mono">
+        <a
+          href={githubLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex gap-2"
+        >
+          <span className="text-primary">repo</span>
+          <Github className="text-secondary" />
+        </a>
+        <a
+          href={liveLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex gap-2"
+        >
+          <span className="text-primary">site</span>
+          <ExternalLink className="text-secondary" />
+        </a>
       </div>
     </div>
   );
