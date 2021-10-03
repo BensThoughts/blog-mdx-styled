1f8501f70d8f61c6f794f7037e75892c5f8a4794
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-02 21:01:14 -0700

    added hover effect

--- src/components/Home/ProjectsCard/index.tsx
+++ src/components/Home/ProjectsCard/index.tsx
@@ -6,6 +6,7 @@ import ProjectLinks from './ProjectLinks';
 import Header from './Header';
 
 import {ZeroInbox} from './projects';
+import {useState} from 'react';
 
 
 // row / col / row / col
@@ -17,7 +18,7 @@ const Card = styled.div`
   grid-template-columns: repeat(12, 1fr);
   grid-template-rows: repeat(12, 1fr);
   gap: 0px;
-  height: 600px;
+  /* height: 600px; */
   /* align-items: center; */
   /* height: 350px; */
   /* border-left: 8px;
@@ -25,19 +26,28 @@ const Card = styled.div`
   border-style: solid; */
 `;
 
-const ImageWrap = styled.div`
+const ImageWrap = styled.div<{
+  hovered: boolean
+}>`
   grid-area: 1 / 7 / 7 / -1;
   z-index: 1;
+  transform: ${(props) => props.hovered ? 'translateX(50px)' : 'translateX(0px)'};
+  transition: transform 250ms;
 `;
 
-const AboutWrap = styled.div`
-  grid-area: 2 / 1 / 10 / 7;
+const AboutWrap = styled.div<{
+  hovered: boolean
+}>`
+  grid-area: 2 / 1 / 9 / 8;
   background-color: rgba(var(--color-bg-terminal), 0.6);
   height: 100%;
   z-index: 2;
   border-left-width: 3px;
   border-left-style: solid;
   border-left-color: rgb(var(--color-text-secondary));
+  transform: ${(props) => props.hovered ? 'translateX(-50px)' : 'translateX(0px)'};
+  transition: transform 250ms;
+
 `;
 
 const HeaderWrap = styled.div`
@@ -47,47 +57,47 @@ const HeaderWrap = styled.div`
 
 // row / col / row / col
 
-const TechWrap = styled.div`
-  grid-area: 7 / 6 / -1 / -1;
+const TechWrap = styled.div<{
+  hovered: boolean
+}>`
+  grid-area: 7 / 6 / -2 / -2;
   background-color: rgba(var(--color-bg-terminal), 0.5);
   height: 100%;
   z-index: 3;
   opacity: 1;
+  transform: ${(props) => props.hovered ? 'translate(128px, 50px)' : 'translateX(0px)'};
+  transition: transform 250ms;
 `;
-
-const ProjectLinksWrap = styled.div`
-  grid-area: 1 / 6 / 2 / 7;
-  display: flex;
-  align-items: center;
-`;
-
 
 export default function ProjectsCard({
 
 }) {
+  const [hovered, setHovered] = useState(false);
   return (
     <>
       <TitleHeader inverse text="Projects"/>
-      <Card className="">
+      <Card className="" onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
         <HeaderWrap>
           <Header />
         </HeaderWrap>
-        <ImageWrap>
+        <ImageWrap hovered={hovered}>
           <ProjectImage
             imgSrc={ZeroInbox.imgSrc}
             imgAlt={ZeroInbox.imgAlt}
             href={ZeroInbox.liveLink}
           />
         </ImageWrap>
-        <AboutWrap>
-          <p className="text-primary text-opacity-100 p-4 font-mono">
-            {ZeroInbox.description}
-          </p>
+        <AboutWrap hovered={hovered}>
+          <div className="p-4 flex flex-col justify-between h-full">
+            <p className="text-primary text-opacity-100 font-mono">
+              {ZeroInbox.description}
+            </p>
+            <div>
+              <ProjectLinks liveLink={ZeroInbox.liveLink} githubLink={ZeroInbox.githubLink} />
+            </div>
+          </div>
         </AboutWrap>
-        <ProjectLinksWrap>
-          <ProjectLinks liveLink={ZeroInbox.liveLink} githubLink={ZeroInbox.githubLink} />
-        </ProjectLinksWrap>
-        <TechWrap className="bg-terminal shadow-xl">
+        <TechWrap hovered={hovered} className="bg-terminal shadow-xl">
           <TechnologiesTerminal technologies={ZeroInbox.technologies} />
         </TechWrap>
       </Card>
