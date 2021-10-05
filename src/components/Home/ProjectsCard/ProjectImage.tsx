b8b1c9581f8942cb1b1c3eab2377c14ff2481ca3
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-05 10:19:40 -0700

    simplified component

--- src/components/Home/ProjectsCard/ProjectImage.tsx
+++ src/components/Home/ProjectsCard/ProjectImage.tsx
@@ -1,43 +1,37 @@
 import styled from '@emotion/styled';
 
-const ImageWrap = styled.div`
-  background-color: rgba(var(--color-image-overlay), 0.7);
-  /* width: 500px;
-  height: 350px; */
-  /* width: 576px;  */
-  /* width: 100%;
-  height: 100%; */
-`;
-
-const A = styled.a`
-  /* width: 576px; */
-  width: 100%;
-  height: 100%;
-  /* width: 500px;
-  height: 350px; */
-  /* background-color: rgba(var(--color-bg-terminal), 1); */
-`;
-
-// #64ffda
-
 const Image = styled.img`
-  /* filter: sepia(50%); */
-  /* height: 350px; */
+  position: relative;
   object-fit: fill;
   /* width: 100%;
   height: 100%; */
   width: 458px;
   height: 300px;
-  /* height: 350px; */
+  z-index: 2;
   mix-blend-mode: multiply;
-  filter: grayscale(100%) contrast(1) brightness(90%);
-  /* width: 576px; */
-  transition: filter mix-blend-mode 1s;
+  filter: grayscale(100%) contrast(1) brightness(100%);
   &:hover {
     mix-blend-mode: normal;
     filter: grayscale(0%) brightness(100%);
   }
 `;
+
+const Anchor = styled.a`
+  width: 100%;
+  height: 100%;
+
+  &::after {
+    content: "";
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    top: 0;
+    left: 0;
+    background-color: rgba(var(--color-image-overlay), 0.7);
+  }
+`;
+
 
 type ProjectImageProps = {
   imgSrc: string,
@@ -51,13 +45,11 @@ export default function ProjectImage({
   href,
 }: ProjectImageProps) {
   return (
-    <ImageWrap>
-      <A href={href} target="_blank" rel="noopener noreferrer" >
-        <Image
-          alt={imgAlt}
-          src={imgSrc}
-        />
-      </A>
-    </ImageWrap>
+    <Anchor href={href} target="_blank" rel="noopener noreferrer" >
+      <Image
+        alt={imgAlt}
+        src={imgSrc}
+      />
+    </Anchor>
   );
 }
