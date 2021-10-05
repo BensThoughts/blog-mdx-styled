078754c34dc1612344f985ffe41f3fbd54fa1970
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-05 13:45:21 -0700

    fixed gtag manager script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -38,15 +38,11 @@ export default class Document extends NextDocument {
     })()`;
 
     const gtmHead = `
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-5BQBMWT');</script>
-    `;
-    const gtmBody = `
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5BQBMWT"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    })(window,document,'script','dataLayer','GTM-5BQBMWT');
     `;
     return (
       <Html lang="en">
@@ -61,7 +57,14 @@ export default class Document extends NextDocument {
         </Head>
         <body>
           <script dangerouslySetInnerHTML={{__html: setInitialTheme}} />
-          <script dangerouslySetInnerHTML={{__html: gtmBody}} />
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5BQBMWT"
+              height="0"
+              width="0"
+              style={{display: 'none', visibility: 'hidden'}}
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
