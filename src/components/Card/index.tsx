372b49a7f0a2989fb3629fb87904b2a7056f37d3
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-05 21:06:17 -0700

    card with folder top

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 const Container = styled.div`
   border-radius: 5px;
-  height: 100%;
+  border: 3px solid rgb(var(--color-app-secondary));
   transition-property: background, color;
   transition-duration: 300ms;
   transition-timing-function: ease-in-out;
@@ -25,6 +25,42 @@ const Pill = styled.div`
   transition-duration: 300ms; */
 `;
 
+const Tab = styled.div`
+  position: relative;
+  display: block;
+  width: 10rem;
+  height: 2rem;
+  margin-left: 10px;
+  border-top: 4px solid rgb(var(--color-app-secondary));
+  border-top-left-radius: 0.3rem/* 6px */;
+  border-top-right-radius: 0.3rem/* 6px */;
+  /* background-color: rgb(var(--color-app-primary)); */
+  background: linear-gradient(to bottom, rgba(var(--color-app-primary), 1) 0%, rgba(var(--color-app-primary), 1) 50%);
+  /* box-shadow: 0 3px 3px rgba(0, 0, 0, 0.4), inset 0 1px 0 rgba(0, 0, 0, 0.5); */
+  &::before, &::after {
+    position: absolute;
+    bottom: -1px;
+    width:6px;
+    height: 6px;
+    content: " ";
+    border: 1px solid rgba(var(--color-app-primary), 1);
+  }
+
+  &::before {
+    left: -5px;
+    border-bottom-right-radius: 6px;
+    border-width: 0 1px 1px 0;
+    box-shadow: 2px 2px 0 rgba(var(--color-app-primary), 1);
+  }
+
+  &::after {
+    right: -5px;
+    border-bottom-left-radius: 6px;
+    border-width: 0 0 1px 1px;
+    box-shadow: -2px 2px 0 rgba(var(--color-app-primary), 1);
+  }
+`;
+
 type CardProps = {
   subTitle?: string
   title?: string
@@ -41,24 +77,25 @@ export default function Card({
   className,
 }: CardProps) {
   return (
-    <Container className={`h-full relative bg-primary px-2 py-4 md:p-8 flex flex-col justify-start gap-2 shadow-md ${className}`}>
-      {title && <div className="text-2xl text-high-emphesis">{title}</div>}
-      {subTitle && <div className="italic">{subTitle}</div>}
-      {description && <div className="text-base text-secondary">{description}</div>}
-      <div>
-        {children}
-      </div>
-
-      <div className="h-full flex flex-col justify-end">
-        <div className="flex flex-wrap gap-2 justify-start">
-          {tags.map((tag) => (
-            <Pill key={tag}>
-              <span className="text-secondary">#</span>
-              {tag}
-            </Pill>
-          ))}
+    <div className="h-full flex flex-col">
+      <Tab></Tab>
+      <Container className={`h-full relative bg-primary px-2 py-4 md:p-4 flex flex-col justify-start gap-4 shadow-md ${className}`}>
+        <div>
+          {title && <div className="text-2xl text-high-emphesis">{title}</div>}
+          {subTitle && <div className="italic">{subTitle}</div>}
         </div>
-      </div>
-    </Container>
+        {description && <div className="text-base">{description}</div>}
+        <div className="h-full flex flex-col justify-end">
+          <div className="flex flex-wrap gap-2 justify-start">
+            {tags.map((tag) => (
+              <Pill key={tag}>
+                <span className="text-secondary">#</span>
+                {tag}
+              </Pill>
+            ))}
+          </div>
+        </div>
+      </Container>
+    </div>
   );
 }
