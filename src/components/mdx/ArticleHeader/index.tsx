dd9f30d54f0c2e96397dcb06602ccd8ced65ceb3
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-06 14:54:17 -0700

    added tweet link

--- src/components/mdx/ArticleHeader/index.tsx
+++ src/components/mdx/ArticleHeader/index.tsx
@@ -1,10 +1,21 @@
+import styled from '@emotion/styled';
 import Date from '@app/components/mdx/Date';
 import H1 from '@app/components/mdx/H1';
+import AnimatedUnderline from '@app/components/AnimatedUnderline';
+import {Twitter} from '@app/components/Icons/Brands';
+
+const UnderLineController = styled.a`
+  &:hover ${AnimatedUnderline}::after {
+    transform: scaleX(1);
+    opacity: 1;
+  }
+`;
 
 type ArticleHeaderProps = {
   title: string;
   date: string;
   readTime: number;
+  permaLink: string;
   className?: string;
 }
 
@@ -12,20 +23,39 @@ export default function ArticleHeader({
   title,
   date,
   readTime,
+  permaLink,
   className = '',
 }: ArticleHeaderProps) {
+  const tweetMessage = `I'm reading ${title} by @bensthoughts Check it out!\n\n`;
   return (
     <div className={`w-full max-w-4xl mx-auto flex justify-center ${className}`}>
       <div>
-        <H1 className=" md:my-3">
+        <H1 className="md:my-3">
           {title}
         </H1>
-        <div className="flex flex-row justify-between md:flex-col">
-          <Date dateString={date} />
-          <p className="italic font-light">
+        <div className="flex flex-col gap-y-2  md:flex-row md:justify-between">
+          <div className="flex flex-row justify-between md:flex-col">
+            <Date dateString={date} />
+            <p className="italic font-light">
             Read time: {readTime} min.
-          </p>
+            </p>
+          </div>
+          <UnderLineController
+            target="_blank"
+            rel="noreferrer noopener"
+            href={`https://twitter.com/intent/tweet?${new URLSearchParams({
+              url: permaLink,
+              text: tweetMessage,
+            })}`}
+            className="md:self-end inline-block content-center"
+          >
+            <AnimatedUnderline className="text-icon-secondary inline-block content-center mr-2">
+              Tweet this article
+            </AnimatedUnderline>
+            <span className="inline-block content-center pb-1 text-secondary"><Twitter size={20} /></span>
+          </UnderLineController>
         </div>
+
       </div>
     </div>
   );
