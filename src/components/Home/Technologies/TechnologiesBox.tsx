2e10d730625e443b252a6e04e8b9df9f8d70e79b
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-08 16:25:16 -0700

    added some excitement

--- src/components/Home/Technologies/TechnologiesBox.tsx
+++ src/components/Home/Technologies/TechnologiesBox.tsx
@@ -1,3 +1,4 @@
+import ExcitedSmiley from '@app/components/Icons/ExcitedSmiley';
 import styled from '@emotion/styled';
 
 const Background = styled.div`
@@ -16,8 +17,8 @@ export default function TechnologiesBox({className}: AboutBoxProps) {
     <Background className={`bg-terminal bg-opacity-50 w-full max-w-2xl z-10 rounded-sm border-2 border-solid border-opacity-30 p-4 border-secondary shadow-sm ${className}`}>
       <div>
         <p className="my-2">
-        &nbsp;&nbsp;&nbsp;&nbsp;A non-exhaustive but...pretty exhaustive list of the technologies I have been working with recently.
-        Click on the links to go to the home page of a given technology.
+        &nbsp;&nbsp;&nbsp;&nbsp;A list of the technologies I have been excited about <ExcitedSmiley size={30} className="inline-block pb-1 text-secondary" /> and working with recently.
+        Click on the links to go to the home page and learn more about a given technology.
         </p>
       </div>
     </Background>
