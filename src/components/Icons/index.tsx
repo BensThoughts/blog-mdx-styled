e3cf53ca1cd5dd06d7d68b26dee9f861d6fd432e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-08 16:26:00 -0700

    no really, I added the excited smiley emoji

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -4,6 +4,7 @@ export {default as Bars} from './Bars';
 export {default as CloudMoon} from './CloudMoon';
 export {default as Copy} from './Copy';
 export {default as ExternalLink} from './ExternalLink';
+export {default as ExcitedSmiled} from './ExcitedSmiley';
 export {default as Folder} from './Folder';
 export {default as Mail} from './Mail';
 export {default as Monitor} from './Monitor';
