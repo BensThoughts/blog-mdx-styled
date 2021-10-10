04e6b0ef158a1eed88f073b358c2071052ae168a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-10 08:46:51 -0700

    flipped directions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ export default function Home() {
             cloudinaryImgPath={project.cloudinaryImgPath}
             imgAlt={project.imgAlt}
             technologies={project.technologies}
-            reversed={idx % 2 === 0 ? true : false}
+            reversed={idx % 2 === 0 ? false : true}
           />
         ))}
       </GridWrapper>
