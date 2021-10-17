f181cb05082392f4f4762e08718fc682a04a934c
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-17 10:11:34 -0700

    added types

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,6 @@
-type ButtonProps = {
-  children: string;
-  onClick: any;
-  className?: string;
-}
-
-export default function Button({className, children, onClick}: ButtonProps) {
+export default function Button({className, children, ...rest}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button onClick={onClick} className={`bg-secondary font-bold py-2 px-4 rounded inline-flex items-center ${className}`}>
+    <button className={`bg-secondary font-bold py-2 px-4 rounded inline-flex items-center ${className}`} {...rest}>
       {children}
     </button>
   );
