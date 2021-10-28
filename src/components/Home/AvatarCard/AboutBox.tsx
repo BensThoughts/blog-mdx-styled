ec3dbd004e4e5a10c39862cfce1ec2f67e4a0256
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-28 13:37:57 -0700

    fixed spacing

--- src/components/Home/AvatarCard/AboutBox.tsx
+++ src/components/Home/AvatarCard/AboutBox.tsx
@@ -13,17 +13,17 @@ type AboutBoxProps = {
 
 export default function AboutBox({className}: AboutBoxProps) {
   return (
-    <Background className={`bg-terminal bg-opacity-50 w-full max-w-2xl z-10 rounded-sm border-2 border-solid border-opacity-30 p-4 border-secondary shadow-md ${className}`}>
-      <div>
+    <Background className={`bg-primary w-full max-w-2xl z-10 rounded-sm border-2 border-solid border-opacity-30 p-4 border-secondary shadow-md ${className}`}>
+      <div className="leading-7">
         <p>Hi,<br /></p>
-        <p className="my-2">
-        &nbsp;&nbsp;&nbsp;&nbsp;My name is Ben. I am a self-taught software developer with a passion for learning. My path in life has given
+        <p className="my-4">
+        &nbsp;&nbsp;&nbsp;&nbsp;<strong>My name is Ben. I am a self-taught software developer with a passion for learning.</strong> My path in life has given
         me a variety of unique experiences. I have done everything from starting and operating a vertically integrated essential oil company to
         creating my own cloud-based email organizing web application.
         </p>
-        <p className="my-2">
-        &nbsp;&nbsp;&nbsp;&nbsp;I currently spend 20-40 hours a week outside of my full time job studying computer science and learning to develop
-        web applications. As someone that loves learning, programming, and helping people,
+        <p className="my-4">
+        &nbsp;&nbsp;&nbsp;&nbsp;<strong>I currently spend 20-40 hours a week outside of my full time job studying computer science and learning to develop
+        web applications</strong>. As someone that loves learning, programming, and helping people,
         the next logical step for me is to parlay my software development skills into a career. I&apos;m a dreamer that wants to work on massively scalable, human-centric, applications that enhance people&apos;s lives.
         </p>
       </div>
