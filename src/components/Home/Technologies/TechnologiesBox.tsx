1a14b0ba16665751504382cfed46f84c16148f94
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-28 13:56:31 -0700

    added strong tag

--- src/components/Home/Technologies/TechnologiesBox.tsx
+++ src/components/Home/Technologies/TechnologiesBox.tsx
@@ -17,8 +17,13 @@ export default function TechnologiesBox({className}: AboutBoxProps) {
     <Background className={`bg-primary w-full max-w-2xl z-10 rounded-sm border-2 border-solid border-opacity-30 p-4 border-secondary shadow-sm ${className}`}>
       <div>
         <p className="my-2 leading-7">
-        &nbsp;&nbsp;&nbsp;&nbsp;<strong>A list of the technologies I have been excited about <ExcitedSmiley size={30} className="inline-block pb-1 text-icon-secondary" /> and working with recently.</strong>
-        Click on the links to go to the home page and learn more about a given technology.
+        &nbsp;&nbsp;&nbsp;&nbsp;
+          <strong>
+            A list of the technologies I have been excited about&nbsp;
+            <ExcitedSmiley size={30} className="inline-block pb-1 text-icon-secondary" />
+            &nbsp;and working with recently.
+          </strong>
+          &nbsp;&nbsp;Click on the links to go to the home page and learn more about a given technology.
         </p>
       </div>
     </Background>
