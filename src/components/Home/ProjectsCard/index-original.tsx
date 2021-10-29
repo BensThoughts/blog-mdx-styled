5c4754cd4139bf8a936700c3e07b7c4f7210bea3
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-28 20:51:44 -0700

    simplified animations

--- src/components/Home/ProjectsCard/index-original.tsx
+++ src/components/Home/ProjectsCard/index-original.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import {useEffect, useState} from 'react';
-// import {useWindowSize} from 'react-use';
+import {useWindowSize} from 'react-use';
 import {useInView} from 'react-intersection-observer';
 
 import ProjectImage from './ProjectImage';
@@ -12,13 +12,13 @@ const Card = styled.div`
 display: flex;
 flex-direction: column;
 gap: 20px;
-@media (min-width: 1024px) {
-  display: grid;
-  grid-template-columns: repeat(12, 1fr);
-  grid-template-rows: repeat(12, 1fr);
-  gap: 0px;
-  height: 600px;
-  will-change: transform background opacity;
+@media (min-width: 768px) {
+display: grid;
+grid-template-columns: repeat(12, 1fr);
+grid-template-rows: repeat(12, 1fr);
+gap: 0px;
+height: 600px;
+will-change: transform background opacity;
 }
 /* height: 600px; */
 /* align-items: center; */
@@ -39,7 +39,7 @@ transition-property: opacity;
 transition-duration: 250ms;
 will-change: transform background opacity;
 
-@media (min-width: 1024px) {
+@media (min-width: 768px) {
   height: 25px;
   grid-area: ${({reversed}) => reversed ? '1 / 8 / 2 / -1' : '1 / 1 / 2 / 6'};
 }
@@ -48,6 +48,7 @@ will-change: transform background opacity;
 const AboutWrap = styled.div<{
   reversed: boolean,
   viewed: boolean,
+  expanded: boolean,
 }>`
 height: 100%;
 z-index: 2;
@@ -58,41 +59,48 @@ border-style: solid;
 border-color: rgba(var(--color-text-secondary), 1);
 grid-area: 5 / 1 / 9 / -1;
 will-change: transform background opacity;
-opacity: ${({viewed}) => viewed ? 1 : 0};
-transition-property: transform opacity;
-transition-duration: 250ms;
 
-@media (min-width: 1024px) { 
-  grid-area: ${({reversed}) => reversed ? '2 / 6 / 9 / -1' : '2 / 1 / 9 / 8'};
-  transform: ${({viewed, reversed}) => {
+opacity: ${({viewed}) => viewed ? 1 : 0};
+
+transform: ${({viewed, expanded, reversed}) => {
     const flipBit = reversed ? -1 : 1;
-    if (!viewed) {
-      return `translateX(${-200 * flipBit}px)`;
-    } else {
+    if (!viewed || expanded) {
       return `translateX(${-50 * flipBit}px)`;
+    } else {
+      return 'translateX(0)';
     }
   }};
+
+  transition-property: transform opacity;
+  transition-duration: 250ms;
+
+@media (min-width: 768px) { 
+  grid-area: ${({reversed}) => reversed ? '2 / 6 / 9 / -1' : '2 / 1 / 9 / 8'};
 }
 `;
 
 const ImageWrap = styled.div<{
   reversed: boolean,
+  expanded: boolean,
   viewed: boolean,
 }>`
-/* grid-area: 2 / 1 / 5 / -1; */
-display: none;
 z-index: 1;
-@media (min-width: 1024px) {
+grid-area: 2 / 1 / 5 / -1;
+display: none;
+transition-property: transform opacity;
+transition-duration: 250ms;
+will-change: transform background opacity;
+@media (min-width: 768px) {
   display: block;
   width: 100%;
   height: 100%;
   grid-area: ${({reversed}) => reversed ? '1 / 1 / 6 / 7' : '1 / 7 / 6 / -1'};
-  transform: ${({viewed, reversed}) => {
+  transform: ${({viewed, expanded, reversed}) => {
     const flipBit = reversed ? -1 : 1;
-    if (!viewed) {
-      return `translateX(${200 * flipBit}px)`;
+    if (!viewed || expanded) {
+      return `translateX(${flipBit * 50}px)`;
     } else {
-      return `translateX(${50 * flipBit}px)`;
+      return 'translateX(0)';
     }
   }};
   transition-property: transform opacity;
@@ -103,26 +111,26 @@ z-index: 1;
 
 const TechWrap = styled.div<{
   reversed: boolean,
+  expanded: boolean,
   viewed: boolean,
 }>`
 /* height: 100%; */
 z-index: 3;
 opacity: ${({viewed}) => viewed ? 1 : 0};
-/* grid-area: 9 / 1 / -2 / -1; */
-transition-property: transform opacity;
-transition-duration: 250ms;
-will-change: transform background opacity;
-
-@media (min-width: 1024px) {  
-  grid-area: ${({reversed}) => reversed ? '7 / 2 / -1 / 8' : '7 / 6 / -1 / -2'};
-  transform: ${({viewed, reversed}) => {
+grid-area: 9 / 1 / -2 / -1;
+transform: ${({viewed, expanded, reversed}) => {
     const flipBit = reversed ? -1 : 1;
-    if (!viewed) {
-      return `translate(${256 * flipBit}px, 50px)`;
+    if (!viewed || expanded) {
+      return `translate(${flipBit * 128}px, 50px)`;
     } else {
-      return `translate(${64 * flipBit}px, 20px)`;
+      return 'translate(0px, 0px)';
     }
   }};
+  transition-property: transform opacity;
+  transition-duration: 250ms;
+  will-change: transform background opacity;
+@media (min-width: 768px) {  
+  grid-area: ${({reversed}) => reversed ? '7 / 2 / -1 / 8' : '7 / 6 / -1 / -2'};
 }
 `;
 
@@ -154,24 +162,24 @@ export default function ProjectsCard({
   reversed = false,
 }: ProjectCardProps) {
   const {ref, inView} = useInView();
-  // const [hovered, setHovered] = useState(false);
-  // const [focused, setFocused] = useState(false);
-  // const [expanded, setExpanded] = useState(false);
-  // const {width} = useWindowSize();
+  const [hovered, setHovered] = useState(false);
+  const [focused, setFocused] = useState(false);
+  const [expanded, setExpanded] = useState(false);
+  const {width} = useWindowSize();
 
   const [viewed, setViewed] = useState(false);
 
-  // useEffect(() => {
-  //   if (width < 1080) {
-  //     setExpanded(false);
-  //   } else if (focused) {
-  //     setExpanded(true);
-  //   } else if (hovered) {
-  //     setExpanded(true);
-  //   } else {
-  //     setExpanded(false);
-  //   }
-  // }, [width, hovered, focused]);
+  useEffect(() => {
+    if (width < 1080) {
+      setExpanded(false);
+    } else if (focused) {
+      setExpanded(true);
+    } else if (hovered) {
+      setExpanded(true);
+    } else {
+      setExpanded(false);
+    }
+  }, [width, hovered, focused]);
 
   useEffect(() => {
     if (inView) {
@@ -181,10 +189,10 @@ export default function ProjectsCard({
 
   return (
     <Card
-      // onMouseEnter={() => setHovered(true)}
-      // onMouseLeave={() => setHovered(false)}
-      // onFocus={() => setFocused(true)}
-      // onBlur={() => setFocused(false)}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      onFocus={() => setFocused(true)}
+      onBlur={() => setFocused(false)}
       ref={ref}
     >
       <TitleWrap
@@ -196,7 +204,7 @@ export default function ProjectsCard({
 
       <ImageWrap
         reversed={reversed}
-        // expanded={expanded}
+        expanded={expanded}
         viewed={viewed}
         className="shadow-lg"
       >
@@ -214,7 +222,7 @@ export default function ProjectsCard({
       <AboutWrap
         reversed={reversed}
         viewed={viewed}
-        // expanded={expanded}
+        expanded={expanded}
         className={`p-4 flex flex-col justify-between h-full shadow-lg ${reversed ? 'items-end text-right' : 'items-start text-left'}`}
       >
         <p className="text-primary text-opacity-100 font-mono">
@@ -230,7 +238,7 @@ export default function ProjectsCard({
 
       <TechWrap
         reversed={reversed}
-        // expanded={expanded}
+        expanded={expanded}
         viewed={viewed}
       >
         <TechnologiesTerminal technologies={technologies} className="shadow-lg" />
