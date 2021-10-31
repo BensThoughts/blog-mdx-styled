5b9793f908d28b286205dad22915c44823fe810a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-30 21:37:11 -0700

    removed playground

--- src/components/Playground/index.tsx
+++
@@ -1,4 +0,0 @@
-export {default as GrainyGradient} from '@app/components/Playground/GrainyGradient';
-export {default as SVGTerminal} from '@app/components/Playground/SVGTerminal';
-export {default as Kells} from '@app/components/Playground/Kells';
-export {default as Magnify} from '@app/components/Playground/Magnify';
