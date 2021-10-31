57672b84faf7fee9c764c77622250eb48fd0847a
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-10-30 17:55:10 -0700

    added resume download link

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -13,7 +13,9 @@ import {menuItems} from './menuItems';
 import {
   Bars,
   Monitor,
+  Package,
 } from '@app/components/Icons';
+import AnimatedLinkWithIcon from '@app/components/AnimatedLinkWithIcon';
 
 const Nav = styled.nav`
   /* background-color: rgba(0, 0, 0, 0); */
@@ -70,6 +72,13 @@ export default function Navbar({className, ...rest}: NavBarProps) {
               {menuItem.name}
             </MenuItem>
           ))}
+          <a
+            href="assets/Benjamin-Blumenfeld-Jones-Resume-2021.pdf"
+            download="Benjamin Blumenfeld-Jones Resume 2021.pdf"
+            className="over:bg-primary w-full h-10 flex items-center justify-center text-xl text-secondary"
+          >
+            Resume
+          </a>
         </NavLinks>
       </MenuDrawer>
       <NavHider>
@@ -83,6 +92,13 @@ export default function Navbar({className, ...rest}: NavBarProps) {
                 {menuItems.map((menuItem) => (
                   <MenuItem animatedLink key={menuItem.href} href={menuItem.href}>{menuItem.name}</MenuItem>
                 ))}
+                <AnimatedLinkWithIcon
+                  href="assets/Benjamin-Blumenfeld-Jones-Resume-2021.pdf"
+                  download="Benjamin Blumenfeld-Jones Resume 2021.pdf"
+                  text="Resume"
+                  className="text-secondary"
+                  icon={<Package className="text-secondary" />}
+                />
               </div>
             </div>
             <div className="flex items-center justify-end gap-x-4">
