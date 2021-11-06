1f1a3991c8a0b38daea205cc2ce900276a7d7474
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-11-05 20:41:37 -0700

    added 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,7 @@
+export default function FourOhFour() {
+  return (
+    <section className="w-full h-full flex items-center justify-center text-secondary">
+      404: Page not found
+    </section>
+  );
+}
