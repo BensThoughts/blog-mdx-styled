dd7144fc9ebde44884a138350265b182146d7c1e
Benjamin <bensthoughts@gmail.com>
2021-11-16 09:21:13 -0700

    upgraded to next.js 12

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 module.exports = withBundleAnalyzer({
   // target: 'serverless',
+  swcMinify: true,
   reactStrictMode: true,
   // productionBrowserSourceMaps: true,
   images: {
