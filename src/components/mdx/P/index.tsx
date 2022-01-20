6691839df81c22b133d8a60a4bcc72e9626e6b70
Benjamin <BensThoughts@Gmail.Com>
2022-01-20 11:34:59 -0700

    changed line height

--- src/components/mdx/P/index.tsx
+++ src/components/mdx/P/index.tsx
@@ -2,6 +2,6 @@ import {ReactNode} from 'react';
 
 export default function P(props: { children: ReactNode}) {
   return (
-    <p className="mx-auto w-auto text-base sm:text-lg text-opacity-90 text-primary leading-8">{props.children}</p>
+    <p className="w-auto mx-auto leading-8 text-opacity-90 text-primary">{props.children}</p>
   );
 };
