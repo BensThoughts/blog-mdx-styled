4fbd3bf5dfb99ba73718f0d18b85a36a9c91717a
Benjamin <BensThoughts@Gmail.Com>
2022-01-20 12:02:15 -0700

    changed name of component to avoid confusion

--- src/components/NextLink.tsx
+++ src/components/NextLink.tsx
@@ -1,10 +1,10 @@
-import NextLink, {LinkProps} from 'next/link';
+import Link, {LinkProps} from 'next/link';
 import {FC, HTMLProps, useCallback} from 'react';
 import {useRouter} from 'next/router';
 
 interface FLinkProps extends Omit<HTMLProps<HTMLAnchorElement>, 'href' | 'as'>, LinkProps {}
 
-const Link: FC<FLinkProps> = ({
+const NextLink: FC<FLinkProps> = ({
   children,
   href,
   as,
@@ -20,7 +20,7 @@ const Link: FC<FLinkProps> = ({
   const router = useRouter();
   const handleClick = useCallback(
       (e) => {
-        console.log(href);
+        // console.log(href);
         if ((href as string).startsWith('/#')) {
           e.preventDefault();
 
@@ -30,7 +30,7 @@ const Link: FC<FLinkProps> = ({
           const scrollAbove = () => {
             const destination = document.querySelector(hrefStr.substr(1));
             if (destination) {
-              console.log('IN_DESTINATION');
+              // console.log('IN_DESTINATION');
               const rect = destination.getBoundingClientRect();
               const yOffset = -75;
               const y = rect.top + window.scrollY + yOffset;
@@ -39,7 +39,7 @@ const Link: FC<FLinkProps> = ({
           };
 
           const hrefStr = href as string;
-          console.log('PATHNAME: ' + router.pathname);
+          // console.log('PATHNAME: ' + router.pathname);
           if (router.pathname != '/') {
             router.push(href, undefined, {
               scroll: true,
@@ -54,7 +54,7 @@ const Link: FC<FLinkProps> = ({
       [href, router]
   );
   return (
-    <NextLink
+    <Link
       href={href}
       as={as}
       replace={replace}
@@ -74,8 +74,8 @@ const Link: FC<FLinkProps> = ({
       >
         {children}
       </a>
-    </NextLink>
+    </Link>
   );
 };
 
-export default Link;
+export default NextLink;
