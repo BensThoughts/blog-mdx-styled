4843aa968649c11c66eb67a183c3037d82a26fa3
Benjamin <BensThoughts@Gmail.Com>
2022-02-14 10:12:29 -0700

    upgraded tailwindcss to latest

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,9 @@ const generateColorClass = (variable) => {
 };
 
 module.exports = {
-  purge: [
+  content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
