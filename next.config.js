1db0c1a6becf182feebf25834fffd10d674c88eb
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-18 12:40:52 -0700

    removed @emotion/styled components libraries

--- next.config.js
+++ next.config.js
@@ -4,23 +4,31 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  // target: 'serverless',
-  compiler: {
-    emotion: true,
-  },
-  swcMinify: true,
-  reactStrictMode: true,
-  // productionBrowserSourceMaps: true,
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-  // webpack: (config, {isServer}) => {
-  //   if (!isServer) {
-  //     config.node = {
-  //       fs: 'empty',
-  //     };
-  //   }
-  //   return config;
-  // },
-});
+// const withMdx = require('@next/mdx')();
+
+module.exports =
+      withBundleAnalyzer({
+        // target: 'serverless',
+        // compiler: {
+        //   emotion: true,
+        // },
+        swcMinify: true,
+        reactStrictMode: true,
+        // productionBrowserSourceMaps: true,
+        images: {
+          domains: ['res.cloudinary.com'],
+        },
+        // webpack5: true,
+        // webpack: (config) => {
+        //   config.resolve.fallback = {fs: false};
+        //   return config;
+        // },
+        // webpack: (config, {isServer}) => {
+        //   if (!isServer) {
+        //     config.node = {
+        //       fs: 'empty',
+        //     };
+        //   }
+        //   return config;
+        // },
+      });
