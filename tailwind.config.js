000cd8635eee38d12a59ee35b994e10cef8f6022
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-18 12:41:04 -0700

    added some colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
+const colors = require('tailwindcss/colors');
+
 const generateColorClass = (variable) => {
   return ({opacityValue}) =>
     opacityValue
       ? `rgba(var(--${variable}), ${opacityValue})`
       : `rgb(var(--${variable}))`;
 };
-
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
@@ -16,12 +17,29 @@ module.exports = {
       'secondary': generateColorClass('color-app-secondary'),
       'accent': generateColorClass('color-app-accent'),
       'terminal': generateColorClass('color-bg-terminal'),
+      'error': generateColorClass('color-app-error'),
+      'gradient-primary': generateColorClass('color-gradient-primary'),
+      'gradient-secondary': generateColorClass('color-gradient-secondary'),
+      'image-overlay': generateColorClass('color-image-overlay'),
+      'text-color-primary': generateColorClass('color-text-primary'),
+      'text-color-secondary': generateColorClass('color-text-secondary'),
+
+      'statusBarBackgroundColor': generateColorClass('statusBarBackgroundColor'),
+      'statusBarAccentColor': generateColorClass('statusBarAccentColor'),
+      'statusBarAccentColorText': generateColorClass('statusBarAccentColorText'),
+      'statusBarTextColorAccent': generateColorClass('statusBarTextColorAccent'),
+      'statusBarTextColor': generateColorClass('statusBarTextColor'),
+
+      'transparent': colors.transparent,
+      'white': colors.white,
     },
     extend: {
       fontFamily: {
         'press-start': ['"Press Start 2P"', 'cursive'],
         'roboto-slab': ['"Roboto Slab"', 'serif'],
         'merriweather': ['"Merriweather"', 'serif'],
+        'inter': ['Inter', 'sans-serif', 'ui-sans-serif', 'system-ui', '-apple-system',
+          'BlinkMacSystemFont'],
       },
       backgroundColor: {
         'app-bg': generateColorClass('color-bg-primary'),
@@ -37,6 +55,7 @@ module.exports = {
         'icon-secondary': generateColorClass('color-app-secondary'),
         'icon-accent': generateColorClass('color-app-accent'),
         'color-inline-code': generateColorClass('color-text-inline-code'),
+        'error': generateColorClass('color-app-error'),
       },
       borderColor: {
         'primary': generateColorClass('color-app-primary'),
