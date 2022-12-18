e01862a5dd0431f92cdf5f87a0e3a09f443b9862
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-18 14:28:21 -0700

    made text fade to secondary color on hover

--- src/components/UnderlineLinkWithIcon.tsx
+++ src/components/UnderlineLinkWithIcon.tsx
@@ -18,11 +18,11 @@ export default function UnderlineLinkWithIcon({
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className={`flex gap-2 justify-center items-center text-primary ${className}`}
+      className={`flex gap-2 justify-center items-center text-primary group ${className}`}
       {...rest}
     >
       {iconPosition === 'left' && <span className="inline-block">{icon}</span>}
-      <span className="underline underline-offset-4 decoration-secondary">{text}</span>
+      <span className="underline underline-offset-4 decoration-secondary group-hover:text-secondary transition-colors duration-150 ease-linear">{text}</span>
       {iconPosition === 'right' && <span className="inline-block">{icon}</span>}
     </a>
   );
