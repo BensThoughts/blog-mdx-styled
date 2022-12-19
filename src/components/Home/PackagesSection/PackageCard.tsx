30fd084b0e5c079432e67c00f464da43ad984495
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2022-12-18 20:44:38 -0700

    fixed safari issue by removing h-full on PackageCard

--- src/components/Home/PackagesSection/PackageCard.tsx
+++ src/components/Home/PackagesSection/PackageCard.tsx
@@ -17,12 +17,12 @@ export default function PackageCard({
       href={href}
       target="_blank"
       rel="noreferrer noopener"
-      className="relative block no-underline w-full h-full rounded-md
+      className="relative block no-underline w-full rounded-md
                  border-2 border-secondary bg-primary/80 transition-all ease-in-out
                  duration-300 hover:-translate-y-3"
       {...rest}
     >
-      <div className="flex flex-col gap-4 justify-start px-2 py-4 h-full md:p-4">
+      <div className="flex flex-col gap-4 justify-start px-2 py-4 md:p-4">
         <div className="flex justify-between">
           <h1 className="text-2xl text-primary">
             {title}
